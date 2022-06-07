27999930fba07059bcc841665cf896300485d17e
agofer <agofer@users.noreply.github.com>
2022-06-07 12:05:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/ping')
 def ping():
-    return 'Pong on steroids again'
+    return 'Pongush on steroids again'
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0')
